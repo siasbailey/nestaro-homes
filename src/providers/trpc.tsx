23687b0716ea0623a789d00dcd4f7094b6db34a5
sagehutchinson6 <sagehutchinson6@gmail.com>
2026-08-21 15:14:34 -0500

    fix: use native credentials include in trpc client for railway cookies

--- src/providers/trpc.tsx
+++ src/providers/trpc.tsx
@@ -18,17 +18,13 @@ const queryClient = new QueryClient({
     },
   },
 });
+
 const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
       url: "/api/trpc",
       transformer: superjson,
-      fetch(input, init) {
-        return globalThis.fetch(input, {
-          ...(init ?? {}),
-          credentials: "include",
-        });
-      },
+      credentials: "include",
     }),
   ],
 });
